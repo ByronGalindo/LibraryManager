13989690db3028e85b4a4edbb9ef90eb9b04c549
ByronGalindo <bahegasu2000@gmail.com>
2024-01-30 15:34:07 -0500

    Update service of Token, add permissions in the response

---
+++ libraryManagment/autenticacion/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['permissions'] = self.user.get_all_permissions()
+        return data
--- libraryManagment/autenticacion/views.py
+++ libraryManagment/autenticacion/views.py
@@ -1,6 +1,7 @@
-# myapp/views.py
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny
+from .serializers import CustomTokenObtainPairSerializer
 
 class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
+    serializer_class = CustomTokenObtainPairSerializer
